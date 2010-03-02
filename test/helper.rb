3d8aa151e4a5f39352d3b8514a053969b6010844
Paul Campbell <paul@rslw.com>
2010-03-02 18:06:27 +0000

    Initial commit to paul-is-awesome.

---
+++ test/helper.rb
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'test/unit'
+require 'shoulda'
+
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+require 'paul-is-awesome'
+
+class Test::Unit::TestCase
+end
